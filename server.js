8ded1da2e67501f6a504b817228c31dcb8b35e03
Kristina <kristinashevchenko03@gmail.com>
2018-09-10 22:40:02 +0300

    upd server

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require("express");
-//const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const app = express();
 
-//app.use(cookieParser());
-
-app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.listen("3030", () => {
     console.log("Server is running");
 });
